707846576736c188a2ddc544d46a4e80218f71d5
tatianazapata88 <70298165+tatianazapata88@users.noreply.github.com>
2020-12-01 14:19:32 -0500

    index tienda
    index tienda

---
+++ index.php
@@ -0,0 +1,86 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>GPS TECH</title>
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
+        <link rel="stylesheet" href="estilos.css">
+    </head>
+    <body>
+        <header>
+            <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+                <a class="navbar-brand" href="#">BIENVENIDO</a>
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul class="navbar-nav mr-auto">
+                        <li class="nav-item active">
+                            <a class="nav-link" href="calculadora.php">Administración</a>
+                        </li>
+                        <li class="nav-item active">
+                            <a class="nav-link" href="calculadora.php">Catálogo de Productos</a>
+                        </li>
+                    </ul>
+                </div>
+            </nav>
+        </header>
+        <main class="dark">
+        <BR>
+        <h6 class="negritaycolor text-center">GPS TECH</h6>
+        <h2 class="negritaycolor text-center">PARA QUE SIEMPRE SEPAS DONDE ESTÁ</h2>
+        <div class="row text-center">
+        <p class="parrafo1 text-center">GPS TECH te trae un catálogo de productos que te serán de gran utilidad para determinar su ubicación en tiempo real y puedas encontrarlos. Traemos la mejor tecnología para que encuentres lo que tanto buscas, mediante un aliado que ante una pérdida por robo o un descuido estés a un click de hallarlo.  </p>
+</div>    
+      <div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
+        <div class="carousel-inner">
+          <div class="carousel-item active">
+            <img src="img/localiza.jpg" class="img-fluid mx-auto d-block w-100 imagen" alt="imagen1">
+          </div>
+          <div class="carousel-item">
+            <img src="img/mundo.JPG" class="img-fluid mx-auto d-block w-100 imagen" alt="imagen2">
+          </div>
+          <div class="carousel-item">
+            <img src="img/ubicacion.JPG" class="img-fluid mx-auto d-block w-100 imagen " alt="imagen3">
+          </div>
+        </div>
+        <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
+          <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+          <span class="sr-only">Previous</span>
+        </a>
+        <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
+          <span class="carousel-control-next-icon" aria-hidden="true"></span>
+          <span class="sr-only">Next</span>
+        </a>
+      </div>
+    </main>
+    <footer class="bg-dark text-white mt-5">
+        <div class="contaner">
+            <div class="row justify-content-around text-center ">
+                        <div class="col-md-3 mt-5">
+                        <p>Luz Tatiana Zapata ©</p>
+                        <p>tatianazz3@hotmail.com</p>
+                        <p>Medellin - Antioquia</p>
+                    </div>
+                </div>
+                <div class="row justify-content-center text-center">
+                    <div class="col-md-8">
+                        <div class="footer-copyright text-center py-3">© Colombia - 2020 Copyright:
+                            <a
+                                href=" https://www.intersoftware.org.co/">
+                                Grupo Cerrado Intersoftware </a> <a href="https://www.cesde.edu.co/Paginas/tecnicos/procesos-tecnologicos-e-industriales/desarrollo-de-software-virtual.aspx"> - CESDE  </a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+    </footer>
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"
+        integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous">
+    </script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-ho+j7jyWK8fNQe+A12Hb8AhRq26LrZ/JpcUGGOn+Y7RsweNrtN/tE3MoK7ZeZDyx" crossorigin="anonymous">
+    </script>
+</body>
+
+</html>
